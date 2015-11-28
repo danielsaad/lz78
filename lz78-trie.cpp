b3c45f85007d9622ad14cef537cb6b199f0f8798
danielsaad <daniel.saad.nunes@gmail.com>
2015-11-28 16:07:48 -0200

    Working

--- lz78-trie.cpp
+++ lz78-trie.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <unordered_map>
+#include <map>
 #include <cstdint>
 #include <vector>
 
@@ -9,32 +10,60 @@ template <typename T, typename U>
 class hash_pair {
 public:
   	std::size_t operator()(const std::pair<T, U> &x) const {
-    	return std::hash<T>()(x.first) ^ std::hash<U>()(x.second);
+        return std::hash<T>()(x.first)  ^ std::hash<U>()(x.second);
   	}
 };
-void lz78(std::ifstream& input_file, std::ofstream& output_file){
-	std::unordered_map<std::pair<int64_t,char>,int64_t,
-		hash_pair<int64_t,int8_t>> trie;
-	int64_t lz78_factor = 0;
 
+
+template <>
+class hash_pair<uint64_t,uint8_t> {
+public:
+    std::size_t operator()(const std::pair<uint64_t, uint8_t> &x) const {
+        std::cout << "Hash = " << ((std::hash<uint64_t>()(x.first)<<8)  ^ std::hash<uint8_t>()(x.second));
+        return ((std::hash<uint64_t>()(x.first)<<8)  ^ std::hash<uint8_t>()(x.second));
+    }
+};
+
+class cmp{
+public:
+    bool operator()(const std::pair<uint64_t,uint8_t>&a, const std::pair<uint64_t,uint8_t>&  b) const{
+        if(a.first<b.first){
+            return true;
+        }
+        else if(a.first>b.first){
+            return false;
+        }
+        else{
+            return (a.second<b.second);
+        }
+    }
+};
+
+void lz78(std::ifstream& input_file, std::ofstream& output_file){
+//    std::unordered_map<std::pair<uint64_t,uint8_t>,uint64_t,hash_pair<uint64_t,uint8_t>> trie;
+    std::map<std::pair<uint64_t,uint8_t>,uint64_t,cmp> trie;
 	//get length of file
 	input_file.seekg (0, input_file.end);
-    auto length = input_file.tellg();
+    int64_t length = input_file.tellg();
     input_file.seekg (0, input_file.beg);
 
     //allocate buffer and read contents into memory
     std::vector<uint8_t> stream;
+    stream.resize(length);
     input_file.read((char*)stream.data(),length);
 
     //lz78 factoring
     uint64_t new_factor = 1;
-    for(int64_t i=0;i<length;){
-    	int64_t lz78_factor = 0;
-    	for(auto v = trie.find(std::make_pair(lz78_factor,stream[i]));v!=trie.end();i++){
+    for(uint64_t i=0;i<length;){
+        uint64_t lz78_factor = 0;
+        for(auto v = trie.find(std::make_pair(lz78_factor,stream[i]));v!=trie.end();++i){
     		lz78_factor = v->second;
+            v = trie.find(std::make_pair(lz78_factor,stream[i+1]));
     	}
-    	output_file << "<" << lz78_factor << "," << stream[i-1] << "> ";
-    	trie[std::make_pair(lz78_factor,stream[i-1])] = new_factor++;
+    	i++;
+        std::cout << "<" << lz78_factor << "," << stream[i-1] << ">\n";
+        output_file << "<" << lz78_factor << "," << stream[i-1] << "> ";
+        trie[std::make_pair(lz78_factor,stream[i-1])] = new_factor++;
     }
 }
 
